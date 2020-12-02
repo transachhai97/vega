3de14376a74ded8c3957c77b08b0a47ae31c6fd9
HaiTS <transachhai97@gmail.com>
2020-12-02 22:45:27 +0700

    social

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\SocialAccount;
 use App\User;
 use App\Providers\RouteServiceProvider;
 use Carbon\Carbon;
@@ -41,31 +42,35 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware("guest")->except("logout");
     }
 
-    protected function authenticated(Request $request, $user) {
-        if (! $user->status) {
-            toastr()->error("Hey $user->name, Looks like your status is InActive!","InActive");
+    protected function authenticated(Request $request, $user)
+    {
+        if (!$user->status) {
+            toastr()->error(
+                "Hey $user->name, Looks like your status is InActive!",
+                "InActive"
+            );
 
             Auth::logout();
-            return redirect()->route('login');
+            return redirect()->route("login");
         }
 
         Auth::logoutOtherDevices($request->password);
 
         $user->update([
-            'last_login_at' => Carbon::now()->toDateTimeString(),
-            'last_login_ip' => $request->getClientIp()
+            "last_login_at" => Carbon::now()->toDateTimeString(),
+            "last_login_ip" => $request->getClientIp(),
         ]);
 
-        toastr()->info("Hey $user->name, Welcome Back!","Welcome");
+        toastr()->info("Hey $user->name, Welcome Back!", "Welcome");
     }
 
     protected function loggedOut(Request $request)
     {
         // Show success msg.
-        toastr()->success('You have successfully logged out!', 'Logout');
+        toastr()->success("You have successfully logged out!", "Logout");
     }
 
     public function redirectToProvider($provider)
@@ -75,20 +80,33 @@ class LoginController extends Controller
 
     public function handleProviderCallback($provider)
     {
-        $providerUser = Socialite::driver($provider)->stateless()->user();
+        $providerUser = Socialite::driver($provider)
+            ->stateless()
+            ->user();
         $user = User::whereEmail($providerUser->getEmail())->first();
 
         if (!$user) {
             $user = User::create([
-                'email' => $providerUser->getEmail(),
-                'name' => $providerUser->getName(),
-                'password' => Hash::make(12345678),
+                "email" => $providerUser->getEmail(),
+                "name" => $providerUser->getName(),
+                "password" => Hash::make(12345678),
             ]);
         }
 
+        SocialAccount::updateOrCreate(
+            [
+                "social_id" => $providerUser->getId(),
+                "social_provider" => "google",
+            ],
+            ["social_name" => $providerUser->getName(), "user_id" => $user->id]
+        );
+
         Auth::login($user);
 
-        toastr()->success('You have successfully logged in with '.ucfirst($provider).'!', 'Login');
+        toastr()->success(
+            "You have successfully logged in with " . ucfirst($provider) . "!",
+            "Login"
+        );
 
         return redirect($this->redirectPath());
     }
---
+++ app/Models/SocialAccount.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class SocialAccount extends Model
+{
+    protected $fillable = [
+        "user_id",
+        "social_id",
+        "social_provider",
+        "social_name",
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
